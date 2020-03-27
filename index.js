faea0418c73646ec199f94504df44f8e5145e7e6
Lauren Yu <laurenkyu@gmail.com>
2020-03-27 21:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,14 +8,25 @@ class Tree {
   }
 }
 
-class Deciduous extends Tree{
-  constructor(species, name){
+class Deciduous extends Tree {
+  constructor(species, name) {
   super(species)
   this.name = name
   }
   
   static definition() {
     return (super.definition() + " Deciduous trees shed their leaves annually.")
+  }
+}
+
+class Evergreen extendes Tree {
+  constructor(name) {
+    super(species)
+    this.name = name
+  }
+  
+  static definition() {
+    return (super.definitions() + " ")
   }
 }
 
